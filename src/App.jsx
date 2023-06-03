318fc12f6a40d5c0af432a5f8d5563402dbba6e6
Rahim Uddin Rafin <rahim.rafin.1@gmail.com>
2023-06-03 17:07:36 +0600

    product-add-delete-done

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,27 @@ import Main from "./Components/Main";
 
 function App() {
   const [allProducts, setAllProducts] = useState([]);
-  useEffect(() => {
+
+  const searchProducts = () => {
+    event.preventDefault();
+    const search = event.target.search.value;
+    const searchedProducts = allProducts.filter();
+  };
+
+  const allProductsGet = () => {
     fetch("https://fakestoreapi.com/products")
       .then((res) => res.json())
       .then((json) => setAllProducts(json));
+  };
+
+  useEffect(() => {
+    allProductsGet();
   }, []);
 
   return (
     <>
       <Navbar></Navbar>
-      <Main allProducts={allProducts}></Main>
+      <Main allProducts={allProducts} allProductsGet={allProductsGet}></Main>
     </>
   );
 }
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,44 +1,131 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Main = ({ allProducts }) => {
+const Main = ({ allProducts, allProductsGet }) => {
   console.log(allProducts);
+
+  const handleUpdate = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const description = e.target.description.value;
+    const cat = e.target.category.value;
+    const img = e.target.img.value;
+    const price = e.target.price.value;
+
+    fetch("https://fakestoreapi.com/products", {
+      method: "POST",
+      body: JSON.stringify({
+        title: name,
+        price: price,
+        description: description,
+        image: img,
+        category: cat,
+      }),
+    })
+      .then((res) => res.json())
+      .then((json) => console.log(json));
+  };
+
+  const handleEdit = (id) => {
+    fetch(`https://fakestoreapi.com/products/${id}`, {
+      method: "PATCH",
+      body: JSON.stringify({
+        title: "test product",
+        price: 13.5,
+        description: "lorem ipsum set",
+        image: "https://i.pravatar.cc",
+        category: "electronic",
+      }),
+    })
+      .then((res) => res.json())
+      .then((json) => console.log(json));
+  };
+
+  const handleDelete = (id) => {
+    fetch(`https://fakestoreapi.com/products/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((json) => console.log(json));
+  };
+
   return (
-    <div className="container grid xl:grid-cols-[auto_350px] 2xl:grid-cols-[auto_400px] gap-4 2xl:gap-8">
-      <div className="grid grid-cols-2">
-        {allProducts?.map((product) => {
-          return (
-            <>
-              <div className="">
-                <div className=" border">
-                  <div>
-                    <img
-                      style={{
-                        height: "100px",
-                        width: "100px",
-                      }}
-                      src={product?.image}
-                      alt=""
-                    />
-                  </div>
-                  <div>
-                    <h1>{product?.title}</h1>
-                    <p>{product?.price}</p>
-                    <p>{product?.category}</p>
+    <div>
+      <div>
+        <input
+          name="search"
+          type="text"
+          placeholder="Filter products..."
+          class="search"
+          id="lws-searchBook"
+        />
+        <select class="lws-sort" id="lws-sortBook" name="text-input">
+          <option value="">Nothing</option>
+          <option value="asc">Price: Low to High</option>
+          <option value="desc">Price: High to Low</option>
+        </select>
+      </div>
+      <div className="container grid xl:grid-cols-[auto_350px] 2xl:grid-cols-[auto_400px] gap-4 2xl:gap-8">
+        <div className="grid grid-cols-2">
+          {allProducts?.map((product, indx) => {
+            return (
+              <>
+                <div key={indx} className="">
+                  <div className=" border">
+                    <div>
+                      <img
+                        style={{
+                          height: "100px",
+                          width: "100px",
+                        }}
+                        src={product?.image}
+                        alt=""
+                      />
+                    </div>
+                    <div>
+                      <h1>{product?.title}</h1>
+                      <p>{product?.price}</p>
+                      <p>{product?.category}</p>
+                    </div>
+                    <div className="m-4">
+                      <button className="border rounded" type="submit">
+                        Edit
+                      </button>
+                      <button
+                        onClick={() => handleDelete(product.id)}
+                        className="border rounded"
+                        type="submit"
+                      >
+                        Delete
+                      </button>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </>
-          );
-        })}
-      </div>
-      <div>
-        <form className="m-1">
-          <input className="border" placeholder="Product Name" type="text" />
-          <input type="text" />
-          <input type="text" />
-          <input type="text" />
-          <input type="text" />
-        </form>
+              </>
+            );
+          })}
+        </div>
+        <div>
+          <form onSubmit={(e) => handleUpdate(e)} className="m-1">
+            <input
+              name="name"
+              className="border"
+              placeholder="Product Name"
+              type="text"
+            />
+            <input name="description" type="text" placeholder="Description" />
+            <select name="category" id="cars">
+              <option value="electronics">electronics</option>
+              <option value="jewelary">jewelary</option>
+              <option value="mens">mens</option>
+              <option value="womens">womens</option>
+            </select>
+            <input name="img" type="text" placeholder="Image Url" />
+            <input name="price" type="text" placeholder="Price" />
+            <button className="border rounded" type="submit">
+              Add
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
